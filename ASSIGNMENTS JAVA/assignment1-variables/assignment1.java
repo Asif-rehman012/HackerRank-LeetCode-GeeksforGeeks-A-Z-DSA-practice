a9e4beff63f1ee5fda7099a789c7515130ad5fea
Asif Rehman <71745081+Asif-rehman012@users.noreply.github.com>
2024-08-28 10:49:21 +0530

    Add files via upload

---
+++ ASSIGNMENTS JAVA/assignment1-variables/assignment1.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class assignment1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // --------VARIABLES & DATA TYPES QUESTIONS------\\\
+
+        // Question no 1
+
+        // int a = 5;
+        // int b = 4;
+        // int c = 88;
+        // float Average = a + b + c / 3;
+        // System.out.println(Average);
+
+        // ---OR ANother Way (take nos from user)
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        float average = (a + b + c) / 3;
+        System.out.println(average);
+
+        // Question no 2
+
+        // System.out.println("Write down the side of square :--");
+        // int squareside = sc.nextInt();
+        // int AreaOfSquare = squareside * squareside;
+        // System.out.println(AreaOfSquare);
+
+        // Question no 3
+        // System.out.println("price of pencil =");
+        // float penicilCost = sc.nextFloat();
+        // System.out.println("price of pen =");
+        // float penCost = sc.nextFloat();
+        // System.out.println("price of eraser");
+        // float eraserCost = sc.nextFloat();
+        // float TotalCost = (penicilCost + penCost + eraserCost);
+        // System.out.println("Total cost of 3 products = " + TotalCost);
+
+        // float Newtotal = (TotalCost + (TotalCost * 0.18f));
+        // System.out.println("Total New cost with 18% GST added :" + Newtotal);
+
+        // Question no 4
+
+        // byte b = 44;
+        // short s = 77;
+        // char c = 'c';
+        // float f = 4.25f;
+        // double d = 4585;
+
+        // System.out.println(b + s * c + f * d);
+
+        // Question 5 error or not
+        // int $ = 24;
+        // System.out.println($); // valid name variable can start with_$ alphabet
+
+    }
+}
