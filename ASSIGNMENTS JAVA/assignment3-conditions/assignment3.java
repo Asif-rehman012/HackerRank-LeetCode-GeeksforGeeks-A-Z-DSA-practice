0886c038f8d7dd3b077e08fea1bc46961c69b545
Asif Rehman <71745081+Asif-rehman012@users.noreply.github.com>
2024-09-01 10:40:48 +0530

    Add files via upload

---
+++ ASSIGNMENTS JAVA/assignment3-conditions/assignment3.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class assignment3 {
+    public static void main(String[] args) {
+
+        // Question 1 : Write a Java program to get a number from the user and print
+        // whether it is
+        // positive or negative.
+
+        Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        // if (num >= 0) {
+        // System.out.println("print number is positive");
+        // } else {
+        // System.out.println("number is negative");
+        // }
+
+        // Question 2 : Finish the following code so that it prints You have a fever if
+        // your temperature
+        // is above 100 and otherwise prints You don't have a fever.
+
+        // double temp = 99.5;
+        // String fever = (temp >= 103.5) ? "you have a fever" : "you don't have a
+        // fever"; // make it string as it have to store 2 statement lines
+        // System.out.println(fever);
+
+        // Question 3 : Write a Java program to input week number(1-7) and print day of
+        // week name
+        // using switch case.
+        // System.out.println("Print a week number (1-7)");
+        // int number = sc.nextInt();
+
+        // switch (number) {
+        // case 1:
+        // System.out.println("Sunday");
+        // break;
+        // case 2:
+        // System.out.println("Monday");
+        // break;
+        // case 3:
+        // System.out.println("tuesday");
+        // break;
+        // case 4:
+        // System.out.println("wednesday");
+        // break;
+        // case 5:
+        // System.out.println("thrusday");
+        // break;
+        // case 6:
+        // System.out.println("friday");
+        // break;
+        // case 7:
+        // System.out.println("Saturday");
+        // break;
+
+        // default:
+        // System.out.println("NOT A DAY");
+        // break;
+        // }
+
+        // Question 4 : What will be the value of x & y in the following program
+        // int a = 63, b = 36;
+        // boolean x = (a < b) ? true : false;
+        // int y = (a > b) ? a : b;
+        // System.out.println(x); // false cond
+        // System.out.println(y); // value of a is printed
+
+        // Question 5 : Write a Java program that takes a year from the user and print
+        // whether that
+        // year is a leap year or not.
+
+        // int year = sc.nextInt();
+
+        // boolean x = (year % 4) == 0;
+        // boolean y = (year % 100) != 0;
+        // boolean z = ((year % 100 == 0) && (year % 400 == 0));
+        // if (x && (y || z)) {
+        // System.out.println(year + " is a leap year");
+        // } else {
+        // System.out.println(year + " is not a leap year");
+        // }
+
+    }
+}
